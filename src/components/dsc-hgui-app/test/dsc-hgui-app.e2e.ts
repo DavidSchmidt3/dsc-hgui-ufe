4317128bc82f24a06a0c6403634bc55480f058d9
Dávid Schmidt <48670893+DavidSchmidt3@users.noreply.github.com>
2024-03-28 20:11:32 +0100

    remove test

--- src/components/dsc-hgui-app/test/dsc-hgui-app.e2e.ts
+++ src/components/dsc-hgui-app/test/dsc-hgui-app.e2e.ts
@@ -1,11 +1,11 @@
 import { newE2EPage } from '@stencil/core/testing';
 
-describe('dsc-hgui-dsc-hgui-app', () => {
+describe('dsc-hgui-app', () => {
   it('renders', async () => {
     const page = await newE2EPage();
-    await page.setContent('<dsc-hgui-dsc-hgui-app></dsc-hgui-dsc-hgui-app>');
+    await page.setContent('<dsc-hgui-app></dsc-hgui-app>');
 
-    const element = await page.find('dsc-hgui-dsc-hgui-app');
+    const element = await page.find('dsc-hgui-app');
     expect(element).toHaveClass('hydrated');
   });
 });
--- src/components/dsc-hgui-app/test/dsc-hgui.app.spec.tsx
+++ src/components/dsc-hgui-app/test/dsc-hgui.app.spec.tsx
@@ -1,28 +1,8 @@
-import { newSpecPage } from '@stencil/core/testing';
-import { DscHguiApp } from '../dsc-hgui-app';
 
-describe('dsc-hgui-dsc-hgui-app', () => {
+describe('dsc-hgui-app', () => {
 
   it('renders editor', async () => {
-    const page = await newSpecPage({
-      url: `http://localhost/entry/@new`,
-      components: [DscHguiApp],
-      html: `<dsc-hgui-dsc-hgui-app base-path="/"></dsc-hgui-dsc-hgui-app>`,
-    });
-    page.win.navigation = new EventTarget()
-    const child = page.root.shadowRoot.firstElementChild;
-    expect(child.tagName.toLocaleLowerCase()).toEqual("dsc-hgui-dsc-hgui-editor");
 
   });
 
-  it('renders list', async () => {
-    const page = await newSpecPage({
-      url: `http://localhost/dsc-hgui/`,
-      components: [DscHguiApp],
-      html: `<dsc-hgui-dsc-hgui-app base-path="/dsc-hgui/"></dsc-hgui-dsc-hgui-app>`,
-    });
-    page.win.navigation = new EventTarget()
-    const child = page.root.shadowRoot.firstElementChild;
-    expect(child.tagName.toLocaleLowerCase()).toEqual("dsc-hgui-dsc-hgui-list");
-  });
 });
