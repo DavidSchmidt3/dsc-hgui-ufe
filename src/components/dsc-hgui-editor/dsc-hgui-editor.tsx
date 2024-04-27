892a848cc9510297649bde23ae59c145b6fe2adb
Dávid Schmidt <48670893+DavidSchmidt3@users.noreply.github.com>
2024-04-27 10:57:11 +0200

    openapi

--- src/components/dsc-hgui-editor/dsc-hgui-editor.tsx
+++ src/components/dsc-hgui-editor/dsc-hgui-editor.tsx
@@ -11,6 +11,7 @@ export class DscHguiEditor {
   @Prop() apiBase: string;
 
   @Event({ eventName: "editor-closed" }) editorClosed: EventEmitter<string>;
+
   @State() errorMessage: string;
   @State() isValid: boolean;
   @State() entry: any;
@@ -19,39 +20,86 @@ export class DscHguiEditor {
 
   private handleInputEvent(ev: InputEvent): string {
     const target = ev.target as HTMLInputElement;
-    // check validity of elements
-    this.isValid = true;
-    for (let i = 0; i < this.formElement.children.length; i++) {
-      const element = this.formElement.children[i]
-      if ("reportValidity" in element) {
-        const valid = (element as HTMLInputElement).reportValidity();
-        this.isValid &&= valid;
-      }
-    }
     return target.value
   }
 
   private async updateEntry() {
-    // try {
-    //   const api = AmbulanceWaitingListApiFactory(undefined, this.apiBase);
-    //   const response
-    //     = this.entryId === "@new"
-    //       ? await api.createWaitingListEntry(this.ambulanceId, this.entry)
-    //       : await api.updateWaitingListEntry(this.ambulanceId, this.entryId, this.entry);
-    //   if (response.status < 299) {
-    //     this.editorClosed.emit("store")
-    //   } else {
-    //     this.errorMessage = `Cannot store entry: ${response.statusText}`
-    //   }
-    // } catch (err: any) {
-    //   this.errorMessage = `Cannot store entry: ${err.message || "unknown"}`
-    // }
+    for (let i = 0; i < this.formElement.children.length; i++) {
+      const element = this.formElement.children[i]
+      if ("reportValidity" in element) {
+        const valid = (element as HTMLInputElement).reportValidity();
+        if (!valid) {
+          return;
+        }
+      }
+    }
+
+    try {
+      this.entry.createdAt = new Date().toISOString();
+
+      // const api = AmbulanceWaitingListApiFactory(undefined, this.apiBase);
+      // const response
+      //   = this.entryId === "@new"
+      //     ? await api.createWaitingListEntry(this.ambulanceId, this.entry)
+      //     : await api.updateWaitingListEntry(this.ambulanceId, this.entryId, this.entry);
+      // if (response.status < 299) {
+      //   this.editorClosed.emit("store")
+      // } else {
+      //   this.errorMessage = `Cannot store entry: ${response.statusText}`
+      // }
+    } catch (err: any) {
+      this.errorMessage = `Cannot store entry: ${err.message || "unknown"}`
+    }
+  }
+
+  private async getGuidanceEntry(): Promise<any> {
+    if (this.entryId === "new") {
+      this.entry = {
+        id: "new",
+        patientName: "",
+        title: "",
+        description: "",
+        severity: "low",
+        phoneNumber: "",
+        email: "",
+        response: ""
+      }
+      return this.entry;
+    }
+    if (!this.entryId) {
+      return undefined
+    }
+    try {
+      // const response = await AmbulanceOrderListApiFactory(undefined, this.apiBase)
+      //   .getOrderListEntry(this.ambulanceId, this.entryId)
+
+      // if (response.status < 299) {
+      //   this.entry = response.data;
+      // } else {
+      //   this.errorMessage = `Cannot retrieve the order: ${response.statusText}`
+      // }
+    } catch (err: any) {
+      this.errorMessage = `Chyba pri ziskavani zaznamu: ${err.message ?? ""}`
+    }
+    return undefined;
+  }
+
+  async componentWillLoad() {
+    await this.getGuidanceEntry();
   }
 
   render() {
+    if (this.errorMessage) {
+      return (
+        <Host>
+          <div class="error">{this.errorMessage}</div>
+        </Host>
+      )
+    }
+
     return (
       <Host>
-         <form ref={el => this.formElement = el} class="form">
+        <form ref={el => this.formElement = el} class="form">
           <h2>{this.entryId === "new" ? "Nová žiadosť o poradenstvo" : "Úprava žiadosti o poradenstvo"}</h2>
 
           <md-filled-text-field
@@ -60,21 +108,21 @@ export class DscHguiEditor {
             label="Meno pacienta"
             required value={this.entry?.patientName}
             oninput={(ev: InputEvent) => {
-              if (this.entry) { this.entry.orderId = this.handleInputEvent(ev) }
+              if (this.entry) { this.entry.patientName = this.handleInputEvent(ev) }
             }}>
           </md-filled-text-field>
 
           <md-filled-text-field type="text" label="Názov problému" maxlength="255"
-            required value={this.entry?.name}
+            required value={this.entry?.title}
             oninput={(ev: InputEvent) => {
-              if (this.entry) { this.entry.name = this.handleInputEvent(ev) }
+              if (this.entry) { this.entry.title = this.handleInputEvent(ev) }
             }}>
           </md-filled-text-field>
 
           <md-filled-text-field type="textarea" label="Popis problému"
             required value={this.entry?.description}
             oninput={(ev: InputEvent) => {
-              if (this.entry) { this.entry.orderId = this.handleInputEvent(ev) }
+              if (this.entry) { this.entry.description = this.handleInputEvent(ev) }
             }}>
           </md-filled-text-field>
 
@@ -84,7 +132,10 @@ export class DscHguiEditor {
             <md-select-option value="high">Vysoká</md-select-option>
           </md-filled-select>
 
-          <md-filled-text-field type="text" label="Telefónne číslo" maxlength="13"
+          <md-filled-text-field
+            type="text"
+            label="Telefónne číslo"
+            maxlength="13"
             value={this.entry?.phoneNumber}
             oninput={(ev: InputEvent) => {
               if (this.entry) { this.entry.name = this.handleInputEvent(ev) }
@@ -94,7 +145,6 @@ export class DscHguiEditor {
           <md-filled-text-field
             type="text"
             label="Email"
-            maxlength="13"
             value={this.entry?.email}
             oninput={(ev: InputEvent) => {
               if (this.entry) { this.entry.name = this.handleInputEvent(ev) }
